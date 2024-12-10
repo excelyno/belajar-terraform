eade0b917ff453b1d0c8ae4eb38da6230e9256c6
excelyno <excellentqweee@gmail.com>
2024-12-10 21:25:40 +0700

    belajar create storage bucket in google platform

---
+++ terraform/terraform-state-bucket/main.tf
@@ -0,0 +1,17 @@
+resource "google_storage_bucket" "tf_state_bucket"{
+    project = "nasir-441404"
+    name    = "excel-terraform-state"
+    location = "Asia-southeast1"
+    force_destroy = true
+}
+
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    google ={
+        source = "hashicorp/google"
+        version = "~> 5.1.0"
+    }
+  }
+}
